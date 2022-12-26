195a356a81e3e82c7a44bf25aed3f0e8a49de2a9
Marella-Rahal <marellarahhal20@gmail.com>
2022-12-26 13:40:17 +0200

    Shoppingo

---
+++ shoppingo-ui/components/PopUp/NotePopUp.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const showPopUpNote = () => {
+  document.querySelector(".fullScreenNote").style.display = "flex";
+};
+
+const NotePopUp = ({ noteMsg }) => {
+  const closePopUp = () => {
+    document.querySelector(".fullScreenNote").style.display = "none";
+  };
+
+  return (
+    <div className="fullScreenNote hidden fixed z-[100] w-full h-full bg-black/30 justify-center items-center">
+      <div
+        style={{ boxShadow: "0px 0px 5px 5px rgba(255,255,255,1)" }}
+        className="w-full sm:w-2/3 md:w-1/3 mx-4 sm:mx-0 p-5 font-semibold bg-white rounded-lg flex flex-col space-y-5"
+      >
+        {noteMsg}
+
+        <button
+          className="py-1 px-5 rounded-lg self-center hover:scale-[1.1] bg-gradient-to-l from-gradientFrom to-gradientTo"
+          onClick={closePopUp}
+        >
+          إغلاق
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NotePopUp;
--- shoppingo-ui/pages/sellers/locations.js
+++ shoppingo-ui/pages/sellers/locations.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Map from "../../components/Map";
+import NotePopUp, { showPopUpNote } from "../../components/PopUp/NotePopUp";
 
 const Locations = () => {
-  // the location of the user
+  //todo *********** the massage for the popUp
+  const [noteMsg, setNoteMsg] = useState("");
+  //todo ********** the location of the user
   const [coords, setCoords] = useState([]);
-  console.log(coords);
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
         setCoords([pos.coords.longitude, pos.coords.latitude]);
       },
-      (err) => {},
+      (err) => {
+        setNoteMsg(
+          <>
+            <h5 className="text-effectColor text-center">
+              فشلنا في الحصول على موقعك أعد تحميل الصفحة للمحاولة مرة أخرى
+            </h5>
+          </>
+        );
+        showPopUpNote();
+      },
       {
         enableHighAccuracy: true,
         timeout: 30000,
@@ -21,6 +32,7 @@ const Locations = () => {
 
   return (
     <div className="w-full h-screen">
+      <NotePopUp noteMsg={noteMsg} />
       {coords.length > 0 && <Map coords={coords} sellerRoute={true} />}
     </div>
   );
