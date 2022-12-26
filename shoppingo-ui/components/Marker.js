64efa867c89849a12afb0ec53da42f66ef7724cb
Marella-Rahal <marellarahhal20@gmail.com>
2022-12-26 08:12:24 +0200

    Shoppingo

--- shoppingo-ui/components/Marker.js
+++ shoppingo-ui/components/Marker.js
@@ -5,7 +5,7 @@ const Marker = ({ image, color }) => {
     <>
       <button
         type="button"
-        className="marker-btn after:top-[83%] sm:after:top-[85%]"
+        className="marker-btn after:top-[82%] sm:after:top-[85%]"
         style={{ backgroundColor: color, borderColor: color }}
       >
         <img src={image} alt="product image" className="marker-img" />
