252ac6028e335b184ca6dc992b8ba5b49a51d704
Marella Rahal <marellarahhal20@gmail.com>
2023-01-25 16:54:12 +0200

    Shoppingo

--- shoppingo-ui/components/Shop/PriceClassification.js
+++ shoppingo-ui/components/Shop/PriceClassification.js
@@ -3,13 +3,13 @@ import React from "react";
 const PriceClassification = () => {
   return (
     <div className="absolute top-[70px] left-[1px] md:top-[50px] md:-left-[47px] bg-white rounded-lg shadow-md shadow-shadowColor w-[190px] flex-col hidden group-hover:flex text-sm font-bold">
-      <div className="text-textColor rounded-t-lg border-b-2 border-shadowColor/20 hover:bg-textColor hover:text-white p-2 text-center cursor-pointer">
+      <div className="text-textColor rounded-t-lg border-b-2 border-shadowColor/20 hover:bg-textColor hover:text-white dark:hover:bg-darkBgColor p-2 text-center cursor-pointer">
         من المنخفض إلى المرتفع
       </div>
-      <div className="text-textColor border-b-2 border-shadowColor/20 hover:bg-textColor hover:text-white p-2 text-center cursor-pointer">
+      <div className="text-textColor border-b-2 border-shadowColor/20 hover:bg-textColor hover:text-white dark:hover:bg-darkBgColor p-2 text-center cursor-pointer">
         من المرتفع إلى المنخفض
       </div>
-      <div className="text-textColor hover:bg-textColor hover:text-white rounded-b-lg p-2 flex flex-col space-y-2">
+      <div className="text-textColor hover:bg-textColor hover:text-white dark:hover:bg-darkBgColor rounded-b-lg p-2 flex flex-col space-y-2">
         <div className="text-center">من</div>
         <input
           type="number"
