cdd10be158e8f065aaab2032e159591fe2f2f668
Marella Rahal <marellarahhal20@gmail.com>
2023-01-25 17:12:24 +0200

    Shoppingo

--- shoppingo-ui/components/ConfirmSeller/ConfirmSeller.js
+++ shoppingo-ui/components/ConfirmSeller/ConfirmSeller.js
@@ -3,7 +3,7 @@ import React from "react";
 const ConfirmSeller = () => {
   return (
     <>
-      <div className="mx-4 md:mx-8 px-4 rounded-xl bg-gradient-to-l from-gradientFrom to-gradientTo dark:from-darkTextColor2 dark:to-darkTextColor2 flex flex-col-reverse items-center md:flex-row md:justify-between">
+      <div className="mx-4 md:mx-8 px-4 rounded-xl bg-gradient-to-l from-gradientFrom to-gradientTo dark:bg-gradient-to-tr dark:from-darkBgColor dark:to-darkTextColor2 flex flex-col-reverse items-center md:flex-row md:justify-between">
         <div className="flex space-x-3 py-8">
           <button className="bg-white text-effectColor py-2 px-5 xs:px-10 hover:scale-[1.1] shadow-none">
             رفض
