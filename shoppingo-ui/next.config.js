1448d588ec2473a1a7dcac79a7ebecce81ed1756
Marella Rahal <marellarahhal20@gmail.com>
2023-03-30 21:42:34 +0200

    shoppingo

--- shoppingo-ui/next.config.js
+++ shoppingo-ui/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   env: {
     mapbox_key:
-      "pk.eyJ1IjoibWFyZWxsYSIsImEiOiJjbGMxcHJvajMxMWxrM29tenRicm40NGFrIn0.N_jN0uvNlxmzwNfCAVB1Qw",
+      "pk.eyJ1IjoibWFyZWxsYSIsImEiOiJjbGZ2Z2djODUwNzFlM2ZvNzVnYmx1aXRjIn0.STHXakMgCnQ_N-oi5zHO8g",
   },
 };
 
