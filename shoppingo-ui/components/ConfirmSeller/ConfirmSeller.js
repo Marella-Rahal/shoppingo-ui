411dc7621b3ec554480eb85a51e6fb6f01da7232
Marella Rahal <marellarahhal20@gmail.com>
2023-03-30 14:52:40 +0200

    shoppingo

--- shoppingo-ui/components/ConfirmSeller/ConfirmSeller.js
+++ shoppingo-ui/components/ConfirmSeller/ConfirmSeller.js
@@ -1,9 +1,15 @@
 import React from "react";
-
-const ConfirmSeller = () => {
+import { AnimatePresence , motion } from 'framer-motion';
+const ConfirmSeller = (props) => {
   return (
-    <>
-      <div className="mx-4 md:mx-8 px-4 rounded-xl bg-gradient-to-l from-gradientFrom to-gradientTo dark:bg-gradient-to-tr dark:from-darkBgColor dark:to-darkTextColor2 flex flex-col-reverse items-center md:flex-row md:justify-between">
+    <AnimatePresence mode="wait">
+      <motion.div
+      key={props.id}
+      initial={{opacity:0,scale:0}} 
+      animate={{opacity:1,scale:1}} 
+      exit={{opacity:0,scale:0}}
+      transition={{ease:'easeInOut',duration:0.7}}
+      className="mx-4 md:mx-8 px-4 rounded-xl bg-gradient-to-l from-gradientFrom to-gradientTo dark:bg-gradient-to-tr dark:from-darkBgColor dark:to-darkTextColor2 flex flex-col-reverse items-center md:flex-row md:justify-between">
         <div className="flex space-x-3 py-8">
           <button className="bg-white hover:bg-white text-effectColor py-2 px-5 xs:px-10 hover:scale-[1.1] shadow-none">
             رفض
@@ -14,10 +20,10 @@ const ConfirmSeller = () => {
         </div>
 
         <div className="font-bold text-white md xs:text-xl py-8">
-          marellarahhal@gmail.com
+          {props.email}
         </div>
-      </div>
-    </>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
--- shoppingo-ui/pages/profile/confirmSellers.js
+++ shoppingo-ui/pages/profile/confirmSellers.js
@@ -7,10 +7,10 @@ const ConfirmSellers = () => {
     <>
       <Navbar/>
       <div className="pt-28 md:pt-32 pb-10 w-full min-h-screen flex flex-col space-y-5">
-        <ConfirmSeller />
-        <ConfirmSeller />
-        <ConfirmSeller />
-        <ConfirmSeller />
+        <ConfirmSeller id="1" email="marellarahhal@gmail.com"/>
+        <ConfirmSeller id="2" email="marellarahhal@gmail.com"/>
+        <ConfirmSeller id="3" email="marellarahhal@gmail.com"/>
+        <ConfirmSeller id="4" email="marellarahhal@gmail.com"/>
       </div>
     </>
   );
